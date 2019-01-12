22729218066dbe5a506ebbd01f8309baddb360b9
Jasper <bobmugura@gmail.com>
2019-01-11 21:59:56 -0500

    added adding todo item functionality

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Leetcode practice',
         completed: false,
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner prep',
         completed: true,
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Get a haircut',
         completed: false,
       },
@@ -43,12 +45,26 @@ class App extends Component {
     })
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false,
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
-          deleteTodo={this.deleteTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo} />
+        </div>
       </div>
     );
   }
