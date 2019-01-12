feb12052ecca50fd8b694f78f5e3e14965521e68
Jasper <bobmugura@gmail.com>
2019-01-11 20:04:25 -0500

    added todos and item components passing props down

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Leetcode practice',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Dinner prep',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Get a haircut',
+        completed: false,
+      },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Todo App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
